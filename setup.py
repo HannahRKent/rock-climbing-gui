03c32aabd2c7ffd188440cc61f2b853518ac0cb5
Hrosek <hscaer@gmail.com>
2020-09-15 05:14:56 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='rock-climbing-gui',
+    version='1.1',
+    packages=['tests'],
+    url='https://github.com/Hrosek/rock-climbing-gui',
+    license='',
+    author='Hannah Kent',
+    author_email='hscaer@gmail.com',
+    description='A GUI to display information on rock climbing routes'
+)
